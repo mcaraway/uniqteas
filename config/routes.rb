7e82f19965214c7d7a8975a27b10ea597ddb07cf
Michael Caraway <michael.p.caraway@gmail.com>
2012-08-15 15:08:15 -0400

    Fixing assets

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Uniqteas::Application.routes.draw do
 
 
   resources :custom_products, :only => [:new ,:show, :create]
+  
+  match '/blendit', :to=> 'custom_products#new'
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
