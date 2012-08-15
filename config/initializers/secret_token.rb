c99048fc8d05b2a462cf8618d90fee1b58b991df
Michael Caraway <michael.p.caraway@gmail.com>
2012-08-14 20:53:39 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uniqteas::Application.config.secret_token = 'e2eeba67ae6c4dd695929103c7698993a3e5402ac534f6b4d7f60d6f9cb95abf93aa5483dd7accd9479f0b35b57f568ad389aaf04bfe08263d1d436af7df68a7'
