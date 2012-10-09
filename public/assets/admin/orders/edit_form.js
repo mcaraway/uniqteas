28a75a7074091dea670ce2d9e78165a08b68d723
Michael Caraway <michael.p.caraway@gmail.com>
2012-10-09 10:54:20 -0400

    New front page and logout working

--- public/assets/admin/orders/edit_form.js
+++ public/assets/admin/orders/edit_form.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-  $.each($('td.qty input'), function(i, input) {
-
-    $(input).on('change', function() {
-
-      var id = "#" + $(this).attr('id').replace("_quantity", "_id");
-      console.log($(this))
-      console.log($(id))
-
-      jQuery.post("/admin/orders/" + $('input#order_number').val() + "/line_items/" + $(id).val(),
-        { _method: "put", "line_item[quantity]": $(this).val()},
-        function(resp) {
-          $('#order-form-wrapper').html(resp.responseText);
-      })
-    })
-  })
-});
-
+$(document).ready(function(){$.each($("td.qty input"),function(e,t){$(t).on("change",function(){var e="#"+$(this).attr("id").replace("_quantity","_id");console.log($(this)),console.log($(e)),jQuery.post("/admin/orders/"+$("input#order_number").val()+"/line_items/"+$(e).val(),{_method:"put","line_item[quantity]":$(this).val()},function(e){$("#order-form-wrapper").html(e.responseText)})})})});
