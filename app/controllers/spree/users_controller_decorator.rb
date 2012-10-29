0d1406ea0da266529dd20e71d193896694c389bd
Michael Caraway <michael.p.caraway@gmail.com>
2012-10-29 18:46:34 -0400

    Fixed Orders showing up in the account page

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,13 +1,12 @@
 Spree::UsersController.class_eval do
-  def show
+  before_filter :load_myblends, :only => [:show, :myblends]
+  
+  def load_myblends
     @user ||= current_user
     logger.debug "*** user  #{@user.email}"
     @myblends = Kaminari.paginate_array(@user.myproducts).page(params[:page]).per(Spree::Config.products_per_page) 
   end
   
   def myblends
-    @user ||= current_user
-    logger.debug "*** user  #{@user.email}"
-    @myblends = Kaminari.paginate_array(@user.myproducts).page(params[:page]).per(Spree::Config.products_per_page) 
   end
 end
