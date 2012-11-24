1294932521aad0ef82fa3c357e51d208d9a66592
Michael Caraway <michael.p.caraway@gmail.com>
2012-11-24 15:46:50 -0500

    Added summary to cart

--- app/overrides/spree/orders/add_summary_to_cart.rb
+++ app/overrides/spree/orders/add_summary_to_cart.rb
@@ -1,4 +1,4 @@
-# Deface::Override.new(:virtual_path => "spree/orders/edit",
-#                     :name => "add_summary_to_cart",
-#                     :insert_after => "[data-hook='cart_items'], #cart_items[data-hook]",
-#                     :partial => "spree/orders/summary")
+Deface::Override.new(:virtual_path => "spree/orders/edit",
+                     :name => "add_summary_to_cart",
+                     :insert_after => "[data-hook='cart_items'], #cart_items[data-hook]",
+                     :partial => "spree/orders/summary")
