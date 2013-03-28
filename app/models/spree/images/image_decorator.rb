930bb8acd9f452022a9e802ff32ff9b62d299f3d
Michael Caraway <michael.p.caraway@gmail.com>
2013-03-27 20:39:29 -0400

    debug

--- app/models/spree/images/image_decorator.rb
+++ app/models/spree/images/image_decorator.rb
@@ -106,10 +106,9 @@ Spree::Image.class_eval do
   end
 
   def do_download_remote_image(remote_url)
+    logger.debug("****** downloading " + (remote_url.nil?  ? "nil" : remote_url))
     io = open(URI.parse(remote_url))
     def io.original_filename; base_uri.path.split('/').last; end
-    
-    logger.debug("************* io = " + (io.original_filename.blank? ? nil : io.original_filename))
     io.original_filename.blank? ? nil : io
   #rescue # catch url errors with validations instead of exceptions (Errno::ENOENT, OpenURI::HTTPError, etc...)
   end
