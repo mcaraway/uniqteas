66eb6f787b3fe6374722dcdf8d66f35dc766277f
unknown <Michael@.(none)>
2014-02-03 18:21:30 -0500

    fixed default_Scope warning updated migrations fixed devise warning

--- app/models/spree/option_value/option_value_decorator.rb
+++ app/models/spree/option_value/option_value_decorator.rb
@@ -1,6 +1,6 @@
 Spree::OptionValue.class_eval do
 
-  default_scope order("#{quoted_table_name}.position")
+  default_scope {order("#{quoted_table_name}.position")}
 
   scope :for_product, lambda { |product| select("DISTINCT #{table_name}.*").where("spree_option_values_variants.variant_id IN (?)", product.variant_ids).joins(:variants)
   }
