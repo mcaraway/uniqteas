0b124ac69687070e9c5d7911d7ea8089c13380c7
Michael Caraway <michael@carawaytea.com>
2014-02-03 22:59:18 -0500

    removed shipping calculator migration

--- db/update_2_0.sql
+++ db/update_2_0.sql
@@ -10,3 +10,24 @@ delete from spree_option_values_variants where no_delete is null;
 commit;
 
 alter table spree_option_values_variants drop column no_delete;
+
+
+-- fix shipping calculators
+
+update spree_calculators as sc set type = updated.type 
+from (
+select id, concat('Spree::Calculator::Shipping', substring(type, 18, 99)) as type from spree_calculators where calculable_type = 'Spree::ShippingMethod') as updated
+where sc.id = updated.id;
+
+update  users as u
+set     leg_count = aggr.cnt
+,       leg_length = aggr.length
+from    (
+        select  legs.user_id
+        ,       count(*) as cnt
+        ,       sum(length) as length 
+        from    legs 
+        group by
+                legs.user_id
+        ) as aggr
+where   u.user_id = aggr.user_id
