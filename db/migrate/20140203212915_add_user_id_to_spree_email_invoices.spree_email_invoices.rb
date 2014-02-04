c54f581ae4e33415ffd134cd845741e58b874e7d
Michael Caraway <michael@carawaytea.com>
2014-02-03 23:12:22 -0500

    removed shipping calculator migration

--- db/migrate/20140203212915_add_user_id_to_spree_email_invoices.spree_email_invoices.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_email_invoices (originally 20131220162936)
-class AddOrderIdToSpreeEmailInvoices < ActiveRecord::Migration
-  def change
-    add_column :spree_email_invoices, :user_id, :integer
-  end
-end
